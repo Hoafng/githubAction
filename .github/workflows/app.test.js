4d411341d655e401318f662a62446516a23b3b43
Hoang <hoangghcc@gmail.com>
2024-05-20 17:20:37 +0700

    1

---
+++ .github/workflows/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /', () => {
+  it('responds with Hello World!', (done) => {
+    request(app).get('/')
+      .expect('Hello World!', done);
+  });
+});
